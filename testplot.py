40e9982e8e5ef71ae1a244ab6ef3e72a1d46ade7
H R <rahmanak.1@gmail.com>
2019-07-12 13:21:24 +0300

    scatter plot

--- testplot.py
+++ testplot.py
@@ -4,7 +4,7 @@ import numpy as np
 def testscaterplt(x_data, y_data, x_label="", y_label="", title="", color = "b", yscale_log=False):
 
     # plot object
-    _, ax = plt.subplots()
+    _, ax = plot.subplots()
 
     # Plot the data, set the size (s), color and transparency (alpha)
     # of the points
@@ -17,3 +17,8 @@ def testscaterplt(x_data, y_data, x_label="", y_label="", title="", color = "b",
     ax.set_title(title)
     ax.set_xlabel(x_label)
     ax.set_ylabel(y_label)
+    plot.show();
+	
+age = [0,2,4,8,10,12,14,16,18,20,22,24]
+slphrs = [12,12,11,10,10,10,10,9,9,9,9,9]
+testscaterplt(age, slphrs, "Age", "Sleep Hrs", "Age vs Sleep")
